7aa9282ea7958d7108929c1b0dcede353873f118
Vasyl Koperdas <koperdasb@gmail.com>
2023-01-31 10:52:32 +0200

    profile page

--- lib/src/ui/profile/profile_page.dart
+++ lib/src/ui/profile/profile_page.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:geolocation_maps/resources/app_color.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile'),
+    return Center(
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 25),
+            child: CircleAvatar(
+              radius: 90,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50, bottom: 15),
+            child: Text(
+              'koperadsb@gmail.com',
+              style: TextStyle(
+                color: AppColor.emailColor,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Text(
+            'Vasiliy Koperdas',
+            style: TextStyle(
+              color: AppColor.nameColor,
+              fontSize: 28,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
